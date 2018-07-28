cf2e73c0debf86295602b259b65afd4db33ce6e3
guilherme <guile.hm@hotmail.com>
2018-07-28 15:13:43 -0300

    Read another type of table

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import pandas as pd
 orders = pd.read_table('http://bit.ly/chiporders')
 print(orders)
 print(orders.head())
+
+# using another separator, and names, without header
+user_cols = ['user_id', 'age', 'gender', 'occupation', 'zip_code']
+orders = pd.read_table('http://bit.ly/movieusers', sep='|', header=None, names=user_cols)
+print(orders)
+print(orders.head())
