c41737e93cedeb07837faa841aa314f0b154c7fd
guilherme <guile.hm@hotmail.com>
2018-07-28 15:53:09 -0300

    Improve ufo_reports

--- ufo_reports.py
+++ ufo_reports.py
@@ -10,6 +10,19 @@ print(type(ufo.City))
 ufo['Location'] = (ufo.City + ', ' + ufo.State)
 print(ufo.Location)
 
+# renaming columns
 print(ufo.columns)
 ufo.rename(columns={'Colors Reported': 'Colors_Reported', 'Shape Reported': 'Shape_Reported'}, inplace=True)
+print(ufo.columns)
+
+# overwrite columns
+ufo_cols = ['city', 'colors reported', 'shape reported', 'state', 'time', 'location']
+ufo.columns = ufo_cols
+print(ufo.columns)
+
+# overwrite columns while reading file
+ufo = pd.read_csv('http://bit.ly/uforeports', names=ufo_cols)
+
+# replace spaces to underscores in header
+ufo.columns = ufo.columns.str.replace(' ', '_')
 print(ufo.columns)
